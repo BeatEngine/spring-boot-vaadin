7da1f60e62e51d24f3c6c1fdee6f226665d5c547
beatengine <davidgawenus@web.de>
2022-03-02 11:35:55 +0100

    commitAndPush

--- commitAndPush.bat
+++ commitAndPush.bat
@@ -1,6 +1,7 @@
 @echo off
 SET PATH=%PATH%;D:\PortableGit\bin
 git add .
+git pull https://github.com/BeatEngine/spring-boot-vaadin refs/heads/main:upstream/main
 git commit -a -m "commitAndPush"
 git push origin main
 exit
