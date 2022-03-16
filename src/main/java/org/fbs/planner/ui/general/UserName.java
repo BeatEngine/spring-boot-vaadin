6706fa1a18f51d65080267ebb545c54354ed71f0
beatengine <davidgawenus@web.de>
2022-03-16 12:39:48 +0100

    Inputboxen Design.

--- src/main/java/org/fbs/planner/ui/general/UserName.java
+++ src/main/java/org/fbs/planner/ui/general/UserName.java
@@ -6,6 +6,7 @@ public class UserName extends Span
 {
     public UserName(final String name, final String className)
     {
+        this.setId("user-display-name");
         this.addClassName(className);
         this.setText(name);
     }
--- src/main/java/org/fbs/planner/ui/table/Vertretungsplan.java
+++ src/main/java/org/fbs/planner/ui/table/Vertretungsplan.java
@@ -2,12 +2,18 @@ package org.fbs.planner.ui.table;
 
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Input;
 import com.vaadin.flow.component.html.Span;
 import org.fbs.planner.DefinitionPaths;
 import org.fbs.planner.ui.table.components.Th;
 import org.fbs.planner.utils.UtilService;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 @CssImport(DefinitionPaths.CSS_DIR+"table.css")
 public class Vertretungsplan extends Div
@@ -35,11 +41,13 @@ public class Vertretungsplan extends Div
         headTable.addClassName("head-table");
         add(headTable);
         add(table);
-        initForm(LocalDate.now());
+        initForm();
     }
 
-    private void initForm(final LocalDate date)
+    private void initForm()
     {
+        LocalDate mondayOfCurrentWeek = LocalDate.now();
+        mondayOfCurrentWeek = mondayOfCurrentWeek.minusDays(mondayOfCurrentWeek.getDayOfWeek().getValue()-1);
         int lineNumber = 1;
         for(int i = 0; i < 16; i++)
         {
@@ -58,50 +66,74 @@ public class Vertretungsplan extends Div
                 table.getBodyCell(0, i).setText("Abendunterricht");
             }
         }
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        ZoneId defaultZoneId = ZoneId.systemDefault();
         table.getHeadCell(0,0).setText("Stunden");
-        table.getHeadCell(1,0).setText("Montag " + date.toString());
-        table.getHeadCell(2,0).setText("Dienstag " + date.plusDays(1).toString());
-        table.getHeadCell(3,0).setText("Mittwoch " + date.plusDays(2).toString());
-        table.getHeadCell(4,0).setText("Donnerstag " + date.plusDays(3).toString());
-        table.getHeadCell(5,0).setText("Freitag " + date.plusDays(4).toString());
-        table.getHeadCell(6,0).setText("Samstag " + date.plusDays(5).toString());
-
+        table.getHeadCell(1,0).setText("Montag " + dateFormat.format(Date.from(mondayOfCurrentWeek.atStartOfDay(defaultZoneId).toInstant())));
+        table.getHeadCell(2,0).setText("Dienstag " + dateFormat.format(Date.from(mondayOfCurrentWeek.plusDays(1).atStartOfDay(defaultZoneId).toInstant())));
+        table.getHeadCell(3,0).setText("Mittwoch " + dateFormat.format(Date.from(mondayOfCurrentWeek.plusDays(2).atStartOfDay(defaultZoneId).toInstant())));
+        table.getHeadCell(4,0).setText("Donnerstag " + dateFormat.format(Date.from(mondayOfCurrentWeek.plusDays(3).atStartOfDay(defaultZoneId).toInstant())));
+        table.getHeadCell(5,0).setText("Freitag " + dateFormat.format(Date.from(mondayOfCurrentWeek.plusDays(4).atStartOfDay(defaultZoneId).toInstant())));
+        table.getHeadCell(6,0).setText("Samstag " + dateFormat.format(Date.from(mondayOfCurrentWeek.plusDays(5).atStartOfDay(defaultZoneId).toInstant())));
+        final String monday = mondayOfCurrentWeek.toString();
         headTable.getHeadRow(0).removeAll();
         Th title = new Th();
         title.setText("Vertretungsplan / Abwesenheitsmeldung");
         title.getStyle().set("font-size","24pt");
-        title.setWidth("100%");
+        title.setWidth("47%");
         headTable.getHeadRow(0).add(title);
         headTable.getHeadRow(1).removeAll();
         Th headreason = new Th();
-        headreason.setWidth("100%");
+        headreason.setWidth("47%");
         Span name = new Span();
         name.setText("Name:");
-        name.setWidth("33.3333%");
+        name.setWidth("8.3333%");
         name.getStyle().set("display", "inline-block");
-        name.getStyle().set("text-align","left");
+        name.getStyle().set("text-align","center");
+        Input nameInp = new Input();
+        nameInp.setClassName("form-input");
         Span von = new Span();
         von.setText("Von:");
-        von.setWidth("33.3333%");
+        von.setWidth("8.3333%");
         von.getStyle().set("display", "inline-block");
-        von.getStyle().set("text-align","left");
+        von.getStyle().set("text-align","center");
+        Input vonInp = new Input();
+        vonInp.setClassName("form-input");
+        vonInp.setType("date");
+        vonInp.setValue(monday);
+        vonInp.getElement().setAttribute("min", monday);
         Span bis = new Span();
         bis.setText("Bis:");
-        bis.setWidth("33.3333%");
+        bis.setWidth("8.3333%");
         bis.getStyle().set("display", "inline-block");
-        bis.getStyle().set("text-align","left");
+        bis.getStyle().set("text-align","center");
+        Input bisInp = new Input();
+        bisInp.setClassName("form-input");
+        bisInp.setType("date");
+        bisInp.setValue(monday);
+        bisInp.getElement().setAttribute("min", monday);
         headreason.add(name);
+        headreason.add(nameInp);
         headreason.add(von);
+        headreason.add(vonInp);
         headreason.add(bis);
+        headreason.add(bisInp);
         headTable.getHeadRow(1).add(headreason);
 
         headTable.getHeadRow(2).removeAll();
         headTable.getHeadRow(2).add(new Th());
         headTable.getHeadRow(2).add(new Th());
-        headTable.getHeadCell(0, 2).setWidth("70%");
-        headTable.getHeadCell(0, 2).setText("Grund der Abwesenheit:\n\nKlasse:̲ ̲ ̲ ̲ ̲ ̲ ̲ ̲ ̲ ̲ ̲ ̲ ̲  (Belege, wie Einladungen usw. anhängen)");
+        headTable.getHeadCell(0, 2).setWidth("50%");
+        headTable.getHeadCell(0, 2).setText("Grund der Abwesenheit:\n\nKlasse: ");
+        Input classInp = new Input();
+        classInp.setClassName("form-input");
+        Span restText = new Span();
+        restText.setText("(Belege, wie Einladungen usw. anhängen)");
+        headTable.getHeadCell(0, 2).add(classInp);
+        headTable.getHeadCell(0, 2).add(restText);
+
         headTable.getHeadCell(0, 2).getStyle().set("white-space","pre-line");
-        headTable.getHeadCell(0, 2).getStyle().set("width","100%");
+        headTable.getHeadCell(0, 2).getStyle().set("width","47%");
         headTable.getHeadCell(0, 2).getStyle().set("text-align","left");
         Div vermerk = new Div();
         vermerk.setText("Genehmigungsvermerke mit Datum:");
@@ -111,6 +143,8 @@ public class Vertretungsplan extends Div
         vermerk.getStyle().set("white-space","nowrap");
         vermerk.getStyle().set("width","100%");
         vermerk.getStyle().set("text-align","left");
+        Input vermerkInp = new Input();
+        vermerkInp.setClassName("form-input");
         Div abteilungsleiter = new Div();
         abteilungsleiter.setText("Abteilungsleiter/in:");
         abteilungsleiter.setHeight("33.3333%");
@@ -119,6 +153,8 @@ public class Vertretungsplan extends Div
         abteilungsleiter.getStyle().set("white-space","nowrap");
         abteilungsleiter.getStyle().set("width","100%");
         abteilungsleiter.getStyle().set("text-align","left");
+        Input abteilungsleiterInp = new Input();
+        abteilungsleiterInp.setClassName("form-input");
         Div schulleitung = new Div();
         schulleitung.setText("Schulleitung:");
         schulleitung.setHeight("33.3333%");
@@ -127,6 +163,12 @@ public class Vertretungsplan extends Div
         schulleitung.getStyle().set("white-space","nowrap");
         schulleitung.getStyle().set("width","100%");
         schulleitung.getStyle().set("text-align","left");
+        Input schulleitungInp = new Input();
+        schulleitungInp.setClassName("form-input");
+
+        vermerk.add(vermerkInp);
+        abteilungsleiter.add(abteilungsleiterInp);
+        schulleitung.add(schulleitungInp);
 
         Th right = headTable.getHeadCell(1, 2);
         right.add(vermerk);
